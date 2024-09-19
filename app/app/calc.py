65ca7453dc87367bc4a8136ec68a70d5d44d632f
Gabriel Ribeiro Filice Chayb <95705080+gabrielchayb@users.noreply.github.com>
2024-09-19 16:20:44 -0300

    tests working now

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,16 +1,8 @@
 """
-Sample tests
+Calculator functions
 """
-from django.test import SimpleTestCase
-
-from app import calc
 
 
-class CalcTests(SimpleTestCase):
-    """Test the calc module."""
-
-    def test_add_numbers(self):
-        """Test adding numbers together."""
-        res = calc.add(5, 6)
-
-        self.assertEqual(res, 11)
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,16 @@
 """
-Calculator functions
+Sample tests
 """
+from django.test import SimpleTestCase
+
+from app import calc
 
 
-def add(x, y):
-    """Add x and y and return result."""
-    return x + y
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 13)
